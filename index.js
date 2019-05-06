9f31a4c05451d3da132048223aa5448d2c9c5b30
Grant Wasil <grantwasil1@gmail.com>
2019-05-06 01:32:39 -0600

    Added new commands Server/Kick/Prune

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable brace-style */
 /* eslint-disable indent */
 // require the discord.js module
 const Discord = require('discord.js');
@@ -16,13 +17,40 @@ client.once('ready', () => {
 // when the client sees a message, it checks if it's "!ping"
 // if it is, it responds "Pong."
 client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
     if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
     }
     else if (message.content === `${prefix}server`) {
-        message.channel.send(`This server's name is: ${message.guild.name}`);
+        message.channel.send(`Server name: ${message.guild.name}\nTotal Members: ${message.guild.memberCount}`);
+    }
+    else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+    else if (command === 'kick') {
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }
+        const taggedUser = message.mentions.users.first();
+
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }
+    else if (command === 'prune') {
+        const amount = parseInt(args[0]);
+
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        } else if (amount < 2 || amount > 100) {
+            return message.reply('you need to input a number between 2 and 100.');
+        } else {
+            message.channel.bulkDelete(amount);
+        }
     }
 });
 
-// login to Discord with your app's token
+// login to Discord with your app's tokenF
 client.login(token);
